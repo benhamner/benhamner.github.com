cb141c4ea1a200d0193b5b4644b4308ce5c1152d
Ben Hamner <ben@benhamner.com>
2013-02-16 22:57:00 -0600

    Now refactored to use angular.js, still need to fix num questions and sorting

--- StackOverflow/ProductionViewer/app.js
+++ StackOverflow/ProductionViewer/app.js
@@ -12,10 +12,32 @@ StackApp.factory('Question', function($resource) {
     return $resource('http://stackapi.benhamner.com/api/questions');
 });
 
-var QuestionCtrl = function($scope, $location, Question) {
-    var res = Question.get( { q:angular.toJson({order_by:[{field:"close_likelihood", direction:"desc"}],
-                                               filters:[{name:"close_likelihood", op:"is_not_null"}],
-                                               limit:10}) },
-                            function() { $scope.questions = res.objects; });
-    window.question = res;
+StackApp.factory('QuestionEval', function($resource) {
+    return $resource('http://stackapi.benhamner.com/api/eval/questions');
+});
+
+http://stackapi.benhamner.com/api/eval/questions?q={%22functions%22:[{%22name%22:%22count%22,%22field%22:%22id%22}]}
+
+var QuestionCtrl = function($scope, $location, Question, QuestionEval) {
+    $scope.get_num_questions = function() {
+        var query = angular.toJson({functions:[{name: "count",
+                                                field: "id"}]});
+        QuestionEval.get({q: query}, 
+                         function(res) {
+                             $scope.num_questions = res.count__id;
+                         });
+    }
+
+    $scope.update = function() {
+        var query = angular.toJson({order_by:[{field:"close_likelihood", direction:"desc"}],
+                                    filters:[{name:"close_likelihood", op:"is_not_null"}],
+                                    limit:10});
+        Question.get({q: query},
+                     function(questions) { 
+                         $scope.questions = questions.objects; 
+                     });
+        $scope.get_num_questions();
+    }
+
+    $scope.update();
 };
