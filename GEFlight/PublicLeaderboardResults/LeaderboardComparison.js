5e0dcbb1585de1b7648056e1c600fd53f24b1001
Ben Hamner <ben@benhamner.com>
2013-02-20 16:53:05 -0800

    basic d3 table

---
+++ GEFlight/PublicLeaderboardResults/LeaderboardComparison.js
@@ -0,0 +1,33 @@
+d3.csv("PublicLeaderboardResultsByAirlineOnlyFlightsWithEstimatedArrival.csv", function(data) {
+    // the columns you'd like to display
+    var columns = Object.keys(data[0]);
+
+    var table = d3.select("#chart").append("table"),
+        thead = table.append("thead"),
+        tbody = table.append("tbody");
+
+    // append the header row
+    thead.append("tr")
+        .selectAll("th")
+        .data(columns)
+        .enter()
+        .append("th")
+            .text(function(column) { return column; });
+
+    // create a row for each object in the data
+    var rows = tbody.selectAll("tr")
+        .data(data)
+        .enter()
+        .append("tr");
+
+    // create a cell in each row for each column
+    var cells = rows.selectAll("td")
+        .data(function(row) {
+            return columns.map(function(column) {
+                return {column: column, value: row[column]};
+            });
+        })
+        .enter()
+        .append("td")
+            .text(function(d) { return d.value; });
+});
